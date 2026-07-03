a92fc3d8b059a21f4947011a02c42f7512f48a88
AkilaBopage <akilabopage@gmail.com>
2026-07-03 13:46:16 +0530

    correctly comment  the unwanted to test in cores

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,11 @@ dotenv.config();
 
 const app = express();
 
-//app.use(cors({
+/*app.use(cors({
   origin: "https://sricweb-58f5ob76q-akilabopages-projects.vercel.app",
   methods: ["GET", "POST"],
   credentials: true
-//});
+});*/
 app.use(cors());
 app.use(express.json());
 
